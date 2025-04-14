c03fcee38ca267688f28d93e99fd9e7349fe13d3
Nuga25 <osinugaifeoluwa1@gmail.com>
2025-04-14 22:37:08 +0100

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,9 +10,17 @@ module.exports = {
         yellow: " #FFD143",
         lightblue: "rgb(154, 201, 246)",
         bordercolor: " #F0F0F0",
+        planningBg: " #F7F7F7",
+        graytext: ": #BDBDBD",
       },
       fontFamily: {
         manrope: ["Manrope", "san-serif"],
+      },
+      boxShadow: {
+        blueglow: "0px 10px 30px 0px #369FFF66",
+        orangeglow: "0px 10px 30px 0px #FF993A66",
+        greenglow: "0px 10px 30px 0px #8AC53E66",
+        yellowglow: "0px 10px 30px 0px #FFD14366",
       },
     },
   },
